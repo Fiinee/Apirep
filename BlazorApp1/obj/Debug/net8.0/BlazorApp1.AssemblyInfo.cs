80b5eacbb1f3a9adeb5df9c9ab599b4b2394ba38
Fiinee <zhumaeva.ali@gmail.com>
2024-12-11 13:41:31 +0300

    фронт старт врум врум

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e170dfea4df8aebd23532bb3212e91bca6f1446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143b5105b4b2694ec45595e9c85a4c09b8a99532")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
