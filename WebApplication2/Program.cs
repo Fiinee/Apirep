fef0b4cccdb2dd79595f9b2977f5da0f33da393b
Fiinee <zhumaeva.ali@gmail.com>
2024-12-11 21:07:37 +0300

    круд???

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.EntityFrameworkCore;
 using WebApplication2.Models;
+using static System.Net.WebRequestMethods;
 
 namespace WebApplication2
 {
@@ -35,7 +36,7 @@ namespace WebApplication2
                 app.UseSwaggerUI();
             }
 
-            app.UseCors(builder => builder.WithOrigins(new[] { "https://localhost:7157" })
+            app.UseCors(builder => builder.WithOrigins(new[] { "https://localhost:7157", "https://apirep-1.onrender.com" })
             .AllowAnyHeader() 
             .AllowAnyMethod());
             
