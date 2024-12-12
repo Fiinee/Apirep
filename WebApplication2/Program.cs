cff4865225160542e69276e7b7013a9f783948a9
Fiinee <zhumaeva.ali@gmail.com>
2024-12-12 09:48:59 +0300

    fdfd

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -15,14 +15,8 @@ namespace WebApplication2
             builder.Services.AddDbContext<PractikaContext>(options => options.UseSqlServer(builder.Configuration["ConnectionString"]));
 
             // Add services to the container.
-<<<<<<< HEAD
 
             builder.Services.AddControllers();
-=======
-            var app = builder.Build();
-            builder.Services.AddControllers();  
-          
->>>>>>> 97c98d8069af15162de17ee2026d9bf2671ddf9d
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -42,35 +36,20 @@ namespace WebApplication2
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-<<<<<<< HEAD
 
             app.UseCors(builder => builder.WithOrigins(new[] { "https://localhost:7157" })
             .AllowAnyHeader()
             .AllowAnyOrigin()
             .AllowAnyMethod());
-=======
-              app.UseCors(builder => builder.WithOrigins(new[] { "https://localhost:7157", "https://apirep-1.onrender.com" })
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin());
-         
->>>>>>> 97c98d8069af15162de17ee2026d9bf2671ddf9d
 
-
-
-
-           app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
 
             app.MapControllers();
 
-<<<<<<< HEAD
-            app.Run();
-=======
             app.Run();
         }
     }
 }
->>>>>>> 97c98d8069af15162de17ee2026d9bf2671ddf9d
